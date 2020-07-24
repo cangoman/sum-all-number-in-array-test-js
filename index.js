1ba84e6d23cb567fb78d17b344849dc1ca335b46
Camilo <camilogomezmusic@gmail.com>
2020-07-24 16:25:10 +0000

    Completed function to return the sum of all elements in an array, using recursion to handle nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let total = 0;
+  for (let i of array) {
+    if (Array.isArray(i)) {
+      total += sumItems(i);
+    } else
+      total += i;
+  }
+  return total;
 }
 
 module.exports = sumItems;
+
